1525d1f7b88b2162afa6f6768dbdd073998cad49
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-05-26 15:45:09 +0800

    initial psuedocode for assignment 11

---
+++ pseudocode.py
@@ -0,0 +1,5 @@
+#Assignment 11: Convert Assignment 9 and 10 to OOP
+#Since the actual code is already made, i just need to make my previous code into multiple classes
+#create and define the question, quiz, system, and executable classes.
+#very straightforward, make the original code and just define them seperately into their respective classes
+#same process as the previous code, might change a few stuff
