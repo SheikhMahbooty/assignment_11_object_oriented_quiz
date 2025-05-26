1c2cca93c4433e782be611cbebceecbc54fdfb66
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-05-26 17:06:58 +0800

    made the class read the txt file

--- quiz_game.py
+++ quiz_game.py
@@ -2,3 +2,9 @@ class quiz_game:
     def __init__(self, filename="quiz_questions.txt"):
         self.filename = filename
         self.questions = self.load_questions()
+        
+     def load_questions(self):
+        with open(self.filename, "r") as file:
+            lines = file.readlines()
+        questions = []
+        
