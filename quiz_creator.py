719b7a3b9f7e66bcb99349a91f8107a90d3800f1
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-05-26 16:13:37 +0800

    renamed the file to snake case

---
+++ quiz_creator.py
@@ -0,0 +1,24 @@
+class QuizCreator:
+    def __init__(self, filename="quiz_questions.txt"):
+        self.filename = filename
+        
+    def create_question(self):
+        with open(self.filename, "a") as file:
+            while True:
+                question = input("\nEnter a question or type 'exit' to exit: ")
+                if question.lower() == "exit":
+                    break
+                
+                options = [
+                    input("Enter answer a: "),
+                    input("Enter answer b: "),
+                    input("Enter answer c: "),
+                    input("Enter answer d: ")
+                ]
+
+                while True:
+                    correct_letter = input("\nCorrect answer (a/b/c/d): ").lower()
+                    if correct_letter in ['a', 'b', 'c', 'd']:
+                        correct_answer = options['abcd'.index(correct_letter)]
+                        break
+                    print("Invalid input, enter a/b/c/d.")
