dd2262da8c528fe352a7f27b435a8663066f5527
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-05-26 17:09:44 +0800

    used the same function for the program to differentiate the questions with lines using strip

--- quiz_game.py
+++ quiz_game.py
@@ -3,8 +3,14 @@ class quiz_game:
         self.filename = filename
         self.questions = self.load_questions()
         
-     def load_questions(self):
+    def load_questions(self):
         with open(self.filename, "r") as file:
             lines = file.readlines()
         questions = []
-        
+        for i in range(0, len(lines), 6):
+            if i + 5 < len(lines):
+                q_text = lines[i].strip()
+                opts = [lines[i+1].strip(), lines[i+2].strip(), lines[i+3].strip(), lines[i+4].strip()]
+                correct = lines[i+5].strip()
+                questions.append(Question(q_text, opts, correct))
+        return questions
