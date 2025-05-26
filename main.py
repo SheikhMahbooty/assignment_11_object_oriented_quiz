a4fa7c58fba85236a638f41d6fc9d4bb8136e613
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-05-26 17:30:57 +0800

    imported the classes instead of defining init

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-if __name__ == "__main__":
-    print("1. Create quiz questions")
-    print("2. Take the quiz")
-    choice = input("Choose an option (1/2): ")
+from quiz_creator import QuizCreator
+from quiz_game import QuizGame
 
-    if choice == "1":
-        creator = QuizCreator()
-        creator.create_question()
-    elif choice == "2":
-        quiz = QuizGame()
-        quiz.start()
-    else:
-        print("Invalid choice.")
+print("1. Create quiz questions")
+print("2. Take the quiz")
+choice = input("Choose an option (1/2): ")
+
+if choice == "1":
+    creator = QuizCreator()
+    creator.create_question()
+elif choice == "2":
+    quiz = QuizGame()
+    quiz.start()
+else:
+    print("Invalid choice.")
